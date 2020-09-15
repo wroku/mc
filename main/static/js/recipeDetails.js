ed0ba395b084cadbd61785fd7fe52c3e93006ab3
Jakub Wronka <wrokuj@gmail.com>
2020-09-15 23:36:10 +0200

    Responsive ingredient list and js calculating quantities for desired no. servings

---
+++ main/static/js/recipeDetails.js
@@ -0,0 +1,14 @@
+//Function for adding/removing servings
+
+function changeServingsNumber(factor){
+    let servings = parseInt(document.getElementById('numberOfServings').innerHTML);
+    if(!((servings===1) && (factor === -1)) ){
+        document.getElementById('numberOfServings').innerHTML = servings+factor;
+        let quantities =  document.getElementsByClassName("quantity");
+        for(let i=0; i<quantities.length; i++){
+            let newQ = (parseInt(quantities[i].innerHTML)/servings)*(servings+factor);
+            quantities[i].innerHTML = newQ;
+            }
+    }
+}
+
